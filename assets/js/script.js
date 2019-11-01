49b572afe7fc2964072d565461f07aa7e5f87199
Mario Alabi <junioralabi1@gmail.com>
2019-11-01 09:06:23 -0700

    2nd Version

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+
+// initalizes floating button
+  
+$('.fixed-action-btn').floatingActionButton({
+  toolbarEnabled: true 
+  
+});
+
+$(document).ready(function(){
+  $('.tooltipped').tooltip();
+});
+
+// initializes the tooltip function
+
+
+  
+  
+  
+  
